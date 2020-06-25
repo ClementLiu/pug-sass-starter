b55b37c9d63c7ab0ed2c98b60f9aedfc9e846b4b
Clement <ziwei.liu@cca.edu>
2020-06-25 13:44:57 -0700

    add source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,77 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass'),
-    connect = require('gulp-connect'),
-    pug     = require('gulp-pug'),
-    plumber = require('gulp-plumber'),
-    rename  = require('gulp-rename'),
-    uglify  = require('gulp-uglify'),
-    autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  connect = require("gulp-connect"),
+  pug = require("gulp-pug"),
+  plumber = require("gulp-plumber"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify"),
+  sourcemaps = require("gulp-sourcemaps");
+autoprefixer = require("gulp-autoprefixer");
 
 function reload(done) {
   connect.server({
     livereload: true,
-    port: 8080
+    port: 8080,
   });
   done();
 }
 
 function styles() {
-  return (
-    gulp.src('src/sass/styles.sass')
+  return gulp
+    .src("src/sass/styles.sass")
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 3 versions'],
-      cascade: false
-    }))
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(rename('styles.min.css'))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(connect.reload())
-  );
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 3 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("assets/css"))
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(rename("styles.min.css"))
+    .pipe(
+      sourcemaps.write("../maps", { includeContent: false, sourceRoot: "/src" })
+    )
+    .pipe(gulp.dest("assets/css"))
+    .pipe(connect.reload());
 }
 
 function scripts() {
-  return (
-    gulp.src('src/js/scripts.js')
+  return gulp
+    .src("src/js/scripts.js")
     .pipe(plumber())
-    .pipe(gulp.dest('assets/js'))
+    .pipe(gulp.dest("assets/js"))
     .pipe(uglify())
-    .pipe(rename('scripts.min.js'))
-    .pipe(gulp.dest('assets/js'))
-    .pipe(connect.reload())
-  );
+    .pipe(rename("scripts.min.js"))
+    .pipe(gulp.dest("assets/js"))
+    .pipe(connect.reload());
 }
 
 function html() {
-  return (
-    gulp.src('*.html')
-    .pipe(plumber())
-    .pipe(connect.reload())
-  );
+  return gulp.src("*.html").pipe(plumber()).pipe(connect.reload());
 }
 
 function views() {
-  return (
-    gulp.src('src/pug/pages/*.pug')
+  return gulp
+    .src("src/pug/pages/*.pug")
     .pipe(plumber())
-    .pipe(pug({
-        pretty: true
-    }))
-    .pipe(gulp.dest('./'))
-    .pipe(connect.reload())
-  )
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(gulp.dest("./"))
+    .pipe(connect.reload());
 }
 
 function watchTask(done) {
-  gulp.watch('*.html', html);
-  gulp.watch('src/sass/**/*.sass', styles);
-  gulp.watch('src/js/scripts.js', scripts);
-  gulp.watch('src/pug/**/*.pug', views);
+  gulp.watch("*.html", html);
+  gulp.watch("src/sass/**/*.sass", styles);
+  gulp.watch("src/js/scripts.js", scripts);
+  gulp.watch("src/pug/**/*.pug", views);
   done();
 }
 
